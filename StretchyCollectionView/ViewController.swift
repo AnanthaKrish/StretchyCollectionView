8886adaa1bd72a9eccde7c1bf05563f3abe110aa
Ananth <agirijak@in.ibm.com>
2019-04-19 20:37:18 +0530

    initial commit

---
+++ StretchyCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StretchyCollectionView
+//
+//  Created by Anantha Krishnan K G on 19/04/19.
+//  Copyright © 2019 Ananth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
